bd78128d4999b4928fd7788d004c09dd580c9749
Gvn <gvanca.uplisashvili@gmail.com>
2023-01-30 18:04:43 +0400

    second

--- second.js
+++ second.js
@@ -1,7 +1,15 @@
 // Create a simple calculator that can add, subtract, multiply, and divide two numbers using JavaScript.
 
-function calculate() {
-  let num1;
-  let num2;
-  let add = num1 + num2;
+function calculate(operation, n1, n2) {
+  if (operation === "add") {
+    console.log(n1 + n2);
+  } else if (operation === "subtract") {
+    console.log(n1 - n2);
+  } else if (operation === "multiply") {
+    console.log(n1 * n2);
+  } else if (operation === "divide") {
+    console.log(n1 / n2);
+  }
 }
+
+calculate("add", 3, 5);
